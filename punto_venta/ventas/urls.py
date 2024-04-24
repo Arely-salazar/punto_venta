d0b5fe796b582c27351a64c568bb2b3bd358eb52
Arely_SU <al060223@uacam.mx>
2024-04-24 00:49:07 -0600

    Modificadion en las vistas, urls y agregación de inicio.html

--- punto_venta/ventas/urls.py
+++ punto_venta/ventas/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ventas_view, name='Ventas'),
+    path('', views.inicio_view, name='Inicio'),
+    path('ventas/', views.ventas_view, name='Ventas'),
     path('clientes/', views.clientes_view, name='Clientes'),
 ]
--- punto_venta/ventas/views.py
+++ punto_venta/ventas/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def inicio_view(request):
+    return render(request, 'inicio.html')
+
 def ventas_view(request):
     return render(request, 'ventas.html')
 
 def clientes_view(request):
     return render(request, 'clientes.html')
+
